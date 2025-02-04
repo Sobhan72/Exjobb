de27f241cb820c0dd36370ac6edcef54725cffed
zacha <zacha@LAPTOP-96EH7J3U>
2025-02-04 09:21:58 +0100

    4/2

--- main.m
+++ main.m
@@ -1,7 +1,6 @@
 %von Mises deformation plasticity
 clear, clc;
 %Making the mesh
-L = 0.001;
-le = 0.1*L;
-[coord, dof, enod, edof, ex, ey, bc] = designDomain(6*L, 6*L, le); 
-patch(ex',ey',0)
+L = 0.01;
+le = 0.5*L;
+[coord, dof, enod, edof, ex, ey, bc] = designDomain(10*L, 5*L, le); 
