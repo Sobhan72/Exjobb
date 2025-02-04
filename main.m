e8aec6724a389e37b8ff6ef6c724d3ec253150e8
zacha <zacha@LAPTOP-96EH7J3U>
2025-02-04 10:36:02 +0100

    4/2

--- main.m
+++ main.m
@@ -6,8 +6,14 @@ le = 0.5*L;
 lx = 10*L;
 ly = 5*L;
 [coord, dof, enod, edof, ex, ey, bc] = designDomain(lx, ly, le);
-nelm = 
+nelm = (lx/le)*(ly/le);
+%patch(ex', ey', 1)
 
 %Material parameters
-E = 210e9; v = 0.3; sigma_y0 = 360e6; H = 10e9;
-beta = linspace(0,)
+E = 210e9; v = 0.3; sigma_y0 = 360e6; H = 10e9; K = 160e9;
+%beta = linspace(0,50);
+sigma_star = [1,1,1,1,0,0];
+sigma_star_eff = 
+
+function G = shearMod(E, v, )
+G =  E/(2*(1+v));
