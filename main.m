dfc3547b66642df28ab952c05cdde287e8e8e20a
zacha <zacha@LAPTOP-96EH7J3U>
2025-02-04 09:59:28 +0100

    4/2

--- main.m
+++ main.m
@@ -3,4 +3,11 @@ clear, clc;
 %Making the mesh
 L = 0.01;
 le = 0.5*L;
-[coord, dof, enod, edof, ex, ey, bc] = designDomain(10*L, 5*L, le); 
+lx = 10*L;
+ly = 5*L;
+[coord, dof, enod, edof, ex, ey, bc] = designDomain(lx, ly, le);
+nelm = 
+
+%Material parameters
+E = 210e9; v = 0.3; sigma_y0 = 360e6; H = 10e9;
+beta = linspace(0,)
