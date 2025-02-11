38e49484172ccc5c7b3bff2de19412afe981d630
zacha <zacha@LAPTOP-96EH7J3U>
2025-02-11 14:42:33 +0100

    11/2

--- hill.m
+++ hill.m
@@ -1,1 +1,6 @@
 %Hill elastoplastic
+clc, clear all, close all
+E = 210e9; v = 0.3; sig_y0 = 360e6; Hard = 10e9; K = E/(3*(1-2*v));
+F = 1/(2*sig_y0); G = 1/(2*sig_y0); H = 1/(2*sig_y0);
+L = 3/(2*sig_y0); M = 3/(2*sig_y0); N = 3/(2*sig_y0);
+P = [F+G -F -G 0 0 0; -F F+H -H 0 0 0; -G -H G+H 0 0 0; 0 0 0 2*L 0 0; 0 0 0 0 2*M 0; 0 0 0 0 0 2*N];
